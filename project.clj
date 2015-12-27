5e1fb27ffd44cfd896dc54b516dcf7d65f4f0f7e
Jimmy Lu <gongchuo.lu@gmail.com>
2015-12-27 18:11:50 -0500

    Rename from clj-center to cljd

--- project.clj
+++ project.clj
@@ -1,15 +1,9 @@
-(defproject clj-center "0.1.0-SNAPSHOT"
+(defproject cljd "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "https://github.com/Yuhta/clj-center"
+  :url "https://github.com/Yuhta/cljd"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[ch.qos.logback/logback-classic "1.1.3"]
                  [org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.clojure/tools.nrepl "0.2.12"]
-                 [mwdict "0.1.0-SNAPSHOT"]]
-  :plugins [[lein-bin "0.3.5"]]
-  :main ^:skip-aot clj-center.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}}
-  :bin {:name "clj-center"})
+                 [org.clojure/tools.nrepl "0.2.12"]])
--- src/cljd.clj
+++ src/cljd.clj
@@ -1,20 +1,20 @@
-(ns clj-center.core
+(ns cljd
   (:require [clojure.java.io :as io]
             [clojure.tools.logging :as log]
-            [clojure.tools.nrepl.server :refer [start-server]])
+            [clojure.tools.nrepl.server :as server])
   (:import (java.nio.file Files attribute.PosixFilePermission))
   (:gen-class))
 
 (def ^:dynamic *nrepl-file*
-  (io/file (System/getProperty "user.home") ".clj-center-nrepl-port"))
+  (io/file (System/getProperty "user.home") ".cljd-nrepl-port"))
 
-(defn -main []
+(defn start-server []
   (let [nrepl-file (io/file *nrepl-file*)
         nrepl-file-str (str *nrepl-file*)
         _ (when (.exists nrepl-file)
             (throw (RuntimeException. (str "file already exists: "
                                            nrepl-file-str))))
-        server (start-server)]
+        server (server/start-server)]
     (log/debug server)
     (spit nrepl-file (:port server))
     (try
@@ -23,4 +23,5 @@
                                        PosixFilePermission/OWNER_WRITE})
       (catch UnsupportedOperationException _))
     (log/info "write nrepl port info to file: " nrepl-file-str)
-    (.deleteOnExit nrepl-file)))
+    (.deleteOnExit nrepl-file)
+    server))
